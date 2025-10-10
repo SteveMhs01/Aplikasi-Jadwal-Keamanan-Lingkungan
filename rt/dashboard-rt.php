92cff6e8b214fa6dfe7ddbe78fdc2220e446bc14
Rizky <mrizkiifauzi.09@gmail.com>
2025-10-10 21:30:12 +0700

    sidebar dan navbar

--- rt/dashboard-rt.php
+++ rt/dashboard-rt.php
@@ -16,84 +16,11 @@
 </head>
 
 <body class="sb-nav-fixed">
-  <nav class="sb-topnav navbar navbar-expand navbar-dark bg-dark">
-    <!-- Navbar Brand-->
-    <a class="navbar-brand ps-3" href="#">
-      <h3 class="mt-3">Dashboard</h3>
-    </a>
-    <!-- Sidebar Toggle-->
-    <button class="btn btn-link btn-sm order-1 order-lg-0 me-4 me-lg-0" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
-    <!-- Navbar Search-->
-    <form class="d-none d-md-inline-block form-inline ms-auto me-0 me-md-3 my-2 my-md-0">
-      <div class="input-group">
-        <input class="form-control me-2 rounded-pill" type="text" placeholder="Search" aria-label="Search for..." aria-describedby="btnNavbarSearch" />
-        <button class="btn btn-success rounded-circle" id="btnNavbarSearch" type="button"><i class="fas fa-search"></i></button>
-      </div>
-    </form>
-    <!-- Navbar-->
-    <ul class="navbar-nav ms-auto ms-md-0 me-3 me-lg-4">
-      <li class="nav-item dropdown">
-        <a class="nav-link dropdown-toggle" id="navbarDropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false"><i class="fas fa-user fa-fw"></i></a>
-        <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdown">
-          <li>
-            <a class="dropdown-item" href="layout/settings.php">
-              <i class="fas fa-gear me-2"></i>Settings
-            </a>
-          </li>
-          <li>
-            <a class="dropdown-item" href="../login.php" id="logout">
-              <i class="fas fa-sign-out-alt me-2"></i>Logout
-            </a>
-          </li>
-        </ul>
-      </li>
-    </ul>
-  </nav>
+  <?php
+  include 'navbar.php';
+  include 'sidebar.php';
+  ?>
   <div id="layoutSidenav">
-    <div id="layoutSidenav_nav">
-      <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
-        <div class="sb-sidenav-menu">
-          <div class="nav">
-            <div class="sb-sidenav-menu-heading"></div>
-            <a class="nav-link mt-4" href="#">
-              <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
-              Dashboard
-            </a>
-            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
-              <div class="sb-nav-link-icon"><i class="fa-solid fa-user"></i></div>
-              Kelola Akun
-              <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-            </a>
-            <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
-              <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="layout/kelola-akun-sekuriti.php">Akun Sekuriti</a>
-                <a class="nav-link" href="layout/kelola-akun-warga.php">Akun Warga</a>
-              </nav>
-            </div>
-            <a class="nav-link" href="layout/kelola-jadwal.php">
-              <div class="sb-nav-link-icon"><i class="fa-solid fa-calendar-week"></i></div>
-              Kelola Jadwal
-            </a>
-            <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
-              <div class="sb-nav-link-icon"><i class="fa-solid fa-book"></i></div>
-              Laporan
-              <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
-            </a>
-            <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
-              <nav class="sb-sidenav-menu-nested nav">
-                <a class="nav-link" href="layout/laporan-kehadiran.php">Laporan Kehadiran</a>
-                <a class="nav-link" href="layout/laporan-bulanan.php">Laporan Bulanan</a>
-              </nav>
-            </div>
-            <a class="nav-link" href="layout/pengaduan.php">
-              <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
-              Pengaduan
-            </a>
-
-          </div>
-        </div>
-      </nav>
-    </div>
     <div id="layoutSidenav_content">
       <main>
         <div class="container-fluid px-4">
---
+++ warga/sidebar.php
@@ -0,0 +1,47 @@
+<div id="layoutSidenav">
+  <div id="layoutSidenav_nav">
+    <nav class="sb-sidenav accordion sb-sidenav-dark" id="sidenavAccordion">
+      <div class="sb-sidenav-menu">
+        <div class="nav">
+          <div class="sb-sidenav-menu-heading"></div>
+          <a class="nav-link mt-4" href="#">
+            <div class="sb-nav-link-icon"><i class="fas fa-tachometer-alt"></i></div>
+            Dashboard
+          </a>
+          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapseLayouts" aria-expanded="false" aria-controls="collapseLayouts">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-user"></i></div>
+            Kelola Akun
+            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+          </a>
+          <div class="collapse" id="collapseLayouts" aria-labelledby="headingOne" data-bs-parent="#sidenavAccordion">
+            <nav class="sb-sidenav-menu-nested nav">
+              <a class="nav-link" href="layout/kelola-akun-sekuriti.php">Akun Sekuriti</a>
+              <a class="nav-link" href="layout/kelola-akun-warga.php">Akun Warga</a>
+            </nav>
+          </div>
+          <a class="nav-link" href="layout/kelola-jadwal.php">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-calendar-week"></i></div>
+            Kelola Jadwal
+          </a>
+          <a class="nav-link collapsed" href="#" data-bs-toggle="collapse" data-bs-target="#collapsePages" aria-expanded="false" aria-controls="collapsePages">
+            <div class="sb-nav-link-icon"><i class="fa-solid fa-book"></i></div>
+            Laporan
+            <div class="sb-sidenav-collapse-arrow"><i class="fas fa-angle-down"></i></div>
+          </a>
+          <div class="collapse" id="collapsePages" aria-labelledby="headingTwo" data-bs-parent="#sidenavAccordion">
+            <nav class="sb-sidenav-menu-nested nav">
+              <a class="nav-link" href="layout/laporan-kehadiran.php">Laporan Kehadiran</a>
+              <a class="nav-link" href="layout/laporan-bulanan.php">Laporan Bulanan</a>
+            </nav>
+          </div>
+          <a class="nav-link" href="layout/pengaduan.php">
+            <div class="sb-nav-link-icon"><i class="fas fa-table"></i></div>
+            Pengaduan
+          </a>
+
+        </div>
+      </div>
+    </nav>
+  </div>
+
+</div>
