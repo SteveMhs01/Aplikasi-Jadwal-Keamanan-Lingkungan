36eeb4df3de6e6b20f669551125eef7636695268
Nama Kamu <emailkamu@example.com>
2025-10-22 13:07:07 +0700

    menghapus dashboard warga lama ganti ke baru(dari rt)

---
+++ warga/settings.php
@@ -0,0 +1,55 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  <meta name="description" content="" />
+  <meta name="author" content="" />
+  <title>Dashboard</title>
+  <link href="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/style.min.css" rel="stylesheet" />
+  <link href="../css/styles.css" rel="stylesheet" />
+  <link rel="stylesheet" href="sweetalert/sweetalert2.css">
+  <script src="https://use.fontawesome.com/releases/v6.3.0/js/all.js" crossorigin="anonymous"></script>
+  <!-- data tables -->
+  <link rel="stylesheet" href="https://cdn.datatables.net/2.0.3/css/dataTables.bootstrap5.css">
+
+</head>
+
+<body class="sb-nav-fixed" style="background-color: #f8f0f0ff;">
+  <?php
+  
+  include 'sideandnav/sidebar.php';
+  ?>
+  <div id="layoutSidenav">
+    <div id="layoutSidenav_content">
+      <main class="p-4">
+        <div class="container-fluid px-4">
+          <div class="card shadow" style="height: 900px; border-radius: 20px;">
+            <div class="card-body">
+              
+            </div>
+          </div>
+        </div>
+
+          
+      </main>
+
+    </div>
+  </div>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+  <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.11.8/dist/umd/popper.min.js" integrity="sha384-I7E8VVD/ismYTF4hNIPjVp/Zjvgyol6VFvRkX/vR+Vc4jQkC+hVqc2pM8ODewa9r" crossorigin="anonymous"></script>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.8/dist/js/bootstrap.min.js" integrity="sha384-G/EV+4j2dNv+tEPo3++6LCgdCROaejBqfUeNjuKAiuXbjrxilcCdDz6ZAVfHWe1Y" crossorigin="anonymous"></script>
+  <script src="../js/scripts.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/Chart.js/2.8.0/Chart.min.js" crossorigin="anonymous"></script>
+  <script src="../assets/demo/chart-bar-demo.js"></script>
+  <script src="https://cdn.jsdelivr.net/npm/simple-datatables@7.1.2/dist/umd/simple-datatables.min.js" crossorigin="anonymous"></script>
+  <script type="text/javascript" src="../sweetalert/sweetalert2.all.min.js"></script>
+  <!-- data table -->
+  <script src="https://cdn.datatables.net/2.0.3/js/dataTables.js"></script>
+  <script src="https://cdn.datatables.net/2.0.3/js/dataTables.bootstrap5.js"></script>
+
+</body>
+
+</html>
---
+++ warga/sideandnav/navbar.php
@@ -0,0 +1,3 @@
+  <nav class="sb-topnav navbar" id="navbar" style="background-color: #f8f0f0ff;">
+  <button class="btn btn-link btn-xl text-black" id="sidebarToggle" href="#!"><i class="fas fa-bars"></i></button>
+  </nav>
